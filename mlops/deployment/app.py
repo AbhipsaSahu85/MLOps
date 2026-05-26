e1c616c944f80815870e144dab7a2032fdd44645
AbhipsaSahu85 <141187418+AbhipsaSahu85@users.noreply.github.com>
2026-05-26 18:48:30 +0530

    Update app.py

--- mlops/deployment/app.py
+++ mlops/deployment/app.py
@@ -46,4 +46,4 @@ if st.button("Predict"):
     prediction_proba = model.predict_proba(input_data)[0, 1]
     prediction = (prediction_proba >= classification_threshold).astype(int)
     result = "churn" if prediction == 1 else "not churn"
-    st.write(f"Based on the information provided, the customer is likely to {result}.")
+    st.write(f"Based on the information you provided, the customer is likely to {result}.")
